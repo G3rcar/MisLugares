f5c88c9bb92b9110d14363242f175d28474f2d05
Gerardo <g3rcar@gmail.com>
2013-12-07 18:15:37 -0600

    Agregadas todas las librerias y el menu en el MainActivity

--- src/com/g3rdeveloper/lugares/MainActivity.java
+++ src/com/g3rdeveloper/lugares/MainActivity.java
@@ -1,16 +1,22 @@
 package com.g3rdeveloper.lugares;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	private ActionBar ab;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
+        ab = getSupportActionBar();
+        ab.setTitle(getString(R.string.title_mis_lugares));
+        
     }
 
 
